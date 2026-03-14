8828f24e37907e612337716efa19d72c055a3541
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-03-14 02:08:53 +0000

    first case

--- src/gitignore_to_find/gitignoreToFind.ts
+++ src/gitignore_to_find/gitignoreToFind.ts
@@ -10,11 +10,7 @@ export default async function gitignoreToFind(
   const result: string[] = [];
 
   for (const item of list) {
-    // if (item.startsWith("!")) {
-    //   result.push("-path", item);
-    // } else {
-      result.push("-not", "-path", item);
-    // }
+    result.push("-not", "-path", item);
   }
 
   return result;
